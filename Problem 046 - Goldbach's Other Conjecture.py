b841fd7d68435c94f12d3579af2ac065a19dd333
Deviloxide <55717532+Deviloxide@users.noreply.github.com>
2021-07-16 13:07:43 -0500

    Add files via upload

--- Problem 046 - Goldbach's Other Conjecture.py
+++ Problem 046 - Goldbach's Other Conjecture.py
@@ -1,21 +1,3 @@
-def bubble_sort(array):
-    n = len(array)
-
-    for i in range(n):
-        already_sorted = True
-
-        for j in range(n - i - 1):
-            if array[j] > array[j + 1]:
-                array[j], array[j + 1] = array[j + 1], array[j]
-
-                already_sorted = False
-
-        if already_sorted:
-            break
-
-    return array
-
-
 def prime_list(n):
     if n == 1 or n == 2:
         return 0
@@ -33,7 +15,7 @@ def prime_list(n):
 
 def disprove_goldbach(prime_limit, int_limit):
     bach_list = []
-    composite_list = []
+    maaa = []
 
     for i in prime_list(prime_limit):
         for num in range(0, 100):
@@ -41,14 +23,14 @@ def disprove_goldbach(prime_limit, int_limit):
             if ahh % 2 == 1:
                 bach_list.append(ahh)
 
-    bach_list = bubble_sort(list(set(bach_list)))
+    bach_list = sorted(list(set(bach_list)))
 
     for i in range(3, int_limit, 2):
-        composite_list.append(i)
+        maaa.append(i)
 
     for i in range(0, 10000):
-        if composite_list[i] != bach_list[i]:
-            return composite_list[i]
+        if maaa[i] != bach_list[i]:
+            return maaa[i]
 
 
 print(disprove_goldbach(10000, 10000))
