14eccf80482053a9e1fb8db38a9b135217068db6
Deviloxide <55717532+Deviloxide@users.noreply.github.com>
2021-08-03 11:36:44 -0500

    Add files via upload

--- Problem 034 - Digit Factorials.py
+++ Problem 034 - Digit Factorials.py
@@ -1,18 +1,22 @@
-def digit_factorials(start, stop):
+def digit_factorials(limit):
     new_list = []
+    ahh = {"1": 1, "2": 2, "3": 6, "4": 24, "5": 120, "6": 720, "7": 5040, "8": 40320, "9": 362880, "0": 1}
 
-    for num in range(start, stop):
-        sup_total = 0
+    for num in range(0, limit):
+        grand_total = 0
+
         for char in str(num):
-            total = 1
-            end = int(char)
-            for i in range(1, end + 1):
-                total *= i
-            sup_total += total
-        if sup_total == num:
+            grand_total += ahh[char]
+
+        if grand_total == num:
             new_list.append(num)
 
-    return new_list
+    return new_list[2:]
 
 
-print(digit_factorials(0, 100000))
+total = 0
+
+for i in digit_factorials(100000)[2:]:
+    total += i
+
+print(total)
