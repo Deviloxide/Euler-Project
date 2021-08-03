694d99b2cf16c10755dfa1032e0d920e575eb298
Deviloxide <55717532+Deviloxide@users.noreply.github.com>
2021-08-02 21:16:52 -0500

    Add files via upload

---
+++ Problem 097 - Large Non-Mersenne Prime.py
@@ -0,0 +1,7 @@
+def last_digits(base, exp, digits, mult = 1, add = 0):
+    dec = "1" + "0" * digits
+    result = str(mult * pow(base, exp, int(dec)) + add)
+    return int(result[-digits:])
+
+
+print(last_digits(2, 7830457, 10, 28433, 1))
