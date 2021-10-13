958c90dceb2ba994345ba8ae93da402157bf4693
Deviloxide <55717532+Deviloxide@users.noreply.github.com>
2021-10-13 17:59:46 -0500

    Add files via upload

--- Problem 079 - Passcode Derivation.py
+++ Problem 079 - Passcode Derivation.py
@@ -13,24 +13,9 @@ def passcode_derivation(file):
     b8 = []
     b9 = []
 
-    bool_0 = [False]
-    bool_1 = [False]
-    bool_2 = [False]
-    bool_3 = [False]
-    bool_4 = [False]
-    bool_5 = [False]
-    bool_6 = [False]
-    bool_7 = [False]
-    bool_8 = [False]
-    bool_9 = [False]
-
     for line in lines:
         num_list = b0
         num_str = "0"
-        booly = bool_0
-
-        if num_str in line:
-            booly = [True]
 
         if line[2] == num_str:
             if line[0] not in num_list:
@@ -43,10 +28,6 @@ def passcode_derivation(file):
 
         num_list = b1
         num_str = "1"
-        booly = bool_1
-
-        if num_str in line:
-            booly = [True]
 
         if line[2] == num_str:
             if line[0] not in num_list:
@@ -59,10 +40,6 @@ def passcode_derivation(file):
 
         num_list = b2
         num_str = "2"
-        booly = bool_2
-
-        if num_str in line:
-            booly = [True]
 
         if line[2] == num_str:
             if line[0] not in num_list:
@@ -75,10 +52,6 @@ def passcode_derivation(file):
 
         num_list = b3
         num_str = "3"
-        booly = bool_3
-
-        if num_str in line:
-            booly = [True]
 
         if line[2] == num_str:
             if line[0] not in num_list:
@@ -91,10 +64,6 @@ def passcode_derivation(file):
 
         num_list = b4
         num_str = "4"
-        booly = bool_4
-
-        if num_str in line:
-            booly = [True]
 
         if line[2] == num_str:
             if line[0] not in num_list:
@@ -107,10 +76,6 @@ def passcode_derivation(file):
 
         num_list = b5
         num_str = "5"
-        booly = bool_5
-
-        if num_str in line:
-            booly = [True]
 
         if line[2] == num_str:
             if line[0] not in num_list:
@@ -123,10 +88,6 @@ def passcode_derivation(file):
 
         num_list = b6
         num_str = "6"
-        booly = bool_6
-
-        if num_str in line:
-            booly = [True]
 
         if line[2] == num_str:
             if line[0] not in num_list:
@@ -139,10 +100,6 @@ def passcode_derivation(file):
 
         num_list = b7
         num_str = "7"
-        booly = bool_7
-
-        if num_str in line:
-            booly = [True]
 
         if line[2] == num_str:
             if line[0] not in num_list:
@@ -155,10 +112,6 @@ def passcode_derivation(file):
 
         num_list = b8
         num_str = "8"
-        booly = bool_8
-
-        if num_str in line:
-            booly = [True]
 
         if line[2] == num_str:
             if line[0] not in num_list:
@@ -171,10 +124,6 @@ def passcode_derivation(file):
 
         num_list = b9
         num_str = "9"
-        booly = bool_9
-
-        if num_str == line[0] or num_str == line[1] or num_str == line[2]:
-            booly = [True]
 
         if line[2] == num_str:
             if line[0] not in num_list:
@@ -185,9 +134,7 @@ def passcode_derivation(file):
             if line[1] not in num_list:
                 num_list.append(line[0])
 
-    super_bool = [bool_0, bool_1, bool_2, bool_3, bool_4, bool_5, bool_6, bool_7, bool_8, bool_9]
     super_list = [b0, b1, b2, b3, b4, b5, b6, b7, b8, b9]
-    mega_list = []
 
     count = 0
     order = {}
@@ -198,10 +145,5 @@ def passcode_derivation(file):
 
     return sorted(order, key=order.get)
 
+
 print(passcode_derivation("Additional Files\p079_keylog.txt"))
-
-#for i in range(0,10):
-#    if super_bool[i][0] == True:
-#        print(super_list)
-#
-
