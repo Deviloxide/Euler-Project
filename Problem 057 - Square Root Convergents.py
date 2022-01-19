bc26bdd6d1efa90e5e7523e77764a32104534ef7
Deviloxide <55717532+Deviloxide@users.noreply.github.com>
2022-01-19 10:27:33 -0600

    Update Problem 057 - Square Root Convergents.py

--- Problem 057 - Square Root Convergents.py
+++ Problem 057 - Square Root Convergents.py
@@ -1,11 +1,12 @@
 import math
 
+
 def square_root_convergents(limit):
     a = 1
     b = 1
     check_count = 0
 
-    for i in range(0,limit):
+    for i in range(0, limit):
         new_a = 2 * b + a
         new_b = b + a
 
@@ -17,5 +18,6 @@ def square_root_convergents(limit):
         b = new_b
 
     return check_count
+
 
 print(square_root_convergents(1000))
